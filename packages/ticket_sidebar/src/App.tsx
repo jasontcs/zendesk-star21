efa26bc2082ba029f3597cb94ed7955f1b832899
Jason Tse <jasont@star21.com.au>
2023-09-06 10:52:19 +1000

    change service font color

--- packages/ticket_sidebar/src/App.tsx
+++ packages/ticket_sidebar/src/App.tsx
@@ -4,7 +4,7 @@ import zafClient from "@app/zendesk/sdk";
 
 import { Button } from '@zendeskgarden/react-buttons';
 import { Body, Cell, Row as TableRow, Table } from '@zendeskgarden/react-tables';
-import { XL, } from '@zendeskgarden/react-typography';
+import { XL, MD, Span } from '@zendeskgarden/react-typography';
 import { Tag } from '@zendeskgarden/react-tags';
 
 import { zafDomain } from "@app/zendesk/common";
@@ -79,15 +79,17 @@ function App() {
             organizationServices
               .filter((service) => service.type)
               .sort((a, b) => Object.values(ServiceType).indexOf(a.type!) - Object.values(ServiceType).indexOf(b.type!))
-              .map((service) =>
-                <TableRow isFocused={false} key={service.id}>
+              .map((service, index) =>
+                <TableRow isFocused={false} key={service.id} isStriped={index % 2 === 0}>
                   <Cell width="100px" style={{ textAlign: "center" }}>
                     <Tag hue={service.color} style={{ width: "100%" }}>
                       <span>{service.typeTitle}</span>
                     </Tag>
                   </Cell>
                   <Cell style={{ textAlign: "right" }}>
-                    {service.name}
+                    <MD isBold>
+                      <Span hue={service.color}>{service.name}</Span>
+                    </MD>
                   </Cell>
                 </TableRow>
               )
