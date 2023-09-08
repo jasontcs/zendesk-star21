c97d7a4307461c54ae1c0d3be3c7e25a79ed5da5
Jason Tse <jasont@star21.com.au>
2023-09-08 14:58:52 +1000

    update margin again on no support services

--- packages/zendesk/components/NoSupportServices.tsx
+++ packages/zendesk/components/NoSupportServices.tsx
@@ -9,25 +9,22 @@ type NoSupportServicesProps = {
 }
 
 const StyledAlert = styled(Alert)`
-    margin: ${p => (p.theme as IGardenTheme).space.xs} ${p => (p.theme as IGardenTheme).space.xs} ${p => (p.theme as IGardenTheme).space.xs} ${p => (p.theme as IGardenTheme).space.xs};
+    margin: ${p => (p.theme as IGardenTheme).space.xs} 0 ${p => (p.theme as IGardenTheme).space.xs} 0;
 `;
 export const NoSupportServices = ({ types }: NoSupportServicesProps) => {
     return (
         <>
             {types.some((type) => type.noService) &&
-                <>
-                    <StyledAlert type="error">
-                        <Title>Warning</Title>
-                        {
-                            types
+                <StyledAlert type="error">
+                    <Title>Warning</Title>
+                    {
+                        types
                             .filter((type) => type.noService)
                             .map((type) =>
                                 <div key={type.key}><Span>{type.noService?.name}</Span></div>
                             )
-                        }
-                    </StyledAlert>
-                </>
-            }
+                    }
+                </StyledAlert>}
         </>
     )
 }
