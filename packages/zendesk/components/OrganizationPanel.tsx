77204298c87a7421f6719d2bb5d9d3e0c543b068
Jason Tse <jasont@star21.com.au>
2023-09-11 15:19:11 +1000

    add org panel ui

--- packages/zendesk/components/OrganizationPanel.tsx
+++ packages/zendesk/components/OrganizationPanel.tsx
@@ -1,5 +1,7 @@
+import { NoSupportServices } from "."
 import { OrganizationEntity } from "../common/entity"
 import { ImportantContactList } from "./ImportantContactList"
+import { OrganizationServices } from "./OrganizationServices"
 
 
 type OrganizationPanelProps = {
@@ -10,6 +12,11 @@ export const OrganizationPanel = ({
     organization,
 }: OrganizationPanelProps) => {
     return (
-        <ImportantContactList importantUsers={organization.importantContacts} ></ImportantContactList>
+        <>
+            <ImportantContactList importantUsers={organization.importantContacts}></ImportantContactList>
+            <p></p>
+            <NoSupportServices types={organization.services}></NoSupportServices>
+            <OrganizationServices organizationServices={organization.services}></OrganizationServices>
+        </>
     )
 }
