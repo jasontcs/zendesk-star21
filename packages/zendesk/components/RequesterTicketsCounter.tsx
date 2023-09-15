9c6ad1c862cb67b01df3c81bf79f3b801ba742db
Jason Tse <jasont@star21.com.au>
2023-09-15 15:07:56 +1000

    add tooltip to all case

--- packages/zendesk/components/RequesterTicketsCounter.tsx
+++ packages/zendesk/components/RequesterTicketsCounter.tsx
@@ -4,12 +4,15 @@ import { TicketEntity, TicketStatusType } from '../common/entity';
 import { Row, Col, Grid } from '@zendeskgarden/react-grid';
 import React from 'react';
 import { zafUtil } from '../common';
+import { Anchor } from '@zendeskgarden/react-buttons';
+import { Tooltip } from '@zendeskgarden/react-tooltips';
 
 type RequesterTicketCounterProps = {
-    tickets: TicketEntity[]
+    tickets: TicketEntity[],
+    titleOnClick?: () => void,
 }
 
-export const RequesterTicketCounter = ({ tickets }: RequesterTicketCounterProps) => {
+export const RequesterTicketCounter = ({ tickets, titleOnClick }: RequesterTicketCounterProps) => {
     const map = tickets.reduce(
         (entryMap, e) => {
             if ((e.status == 'solved' || e.status == 'closed') && zafUtil.numDaysBetween(e.updateAt, new Date()) > 30) {
@@ -19,26 +22,31 @@ export const RequesterTicketCounter = ({ tickets }: RequesterTicketCounterProps)
         },
         new Map<TicketStatusType, number>()
     );
+
+    const solved = map.get('solved') ?? 0 + (map.get('closed') ?? 0)
+
     return (
         <Grid gutters={false}>
             <Row justifyContent='center'>
-                <CounterTile background='#ffb057' count={map.get('new') ?? 0} title='New' color='#703815'></CounterTile>
-                <CounterTile background='#c72a1c' count={map.get('open') ?? 0} title='Open'></CounterTile>
-                <CounterTile background='#3091ec' count={map.get('pending') ?? 0} title='Pending'></CounterTile>
-                <CounterTile background='#2f3941' count={map.get('hold') ?? 0} title='On-hold'></CounterTile>
-                <CounterTile background='#87929d' count={map.get('solved') ?? 0 + (map.get('closed') ?? 0)} title='Solved'></CounterTile>
+                <CounterTile titleOnClick={titleOnClick} background='#ffb057' count={map.get('new') ?? 0} title='New' color='#703815'></CounterTile>
+                <CounterTile titleOnClick={titleOnClick} background='#c72a1c' count={map.get('open') ?? 0} title='Open'></CounterTile>
+                <CounterTile titleOnClick={titleOnClick} background='#3091ec' count={map.get('pending') ?? 0} title='Pending'></CounterTile>
+                <CounterTile titleOnClick={titleOnClick} background='#2f3941' count={map.get('hold') ?? 0} title='On-hold'></CounterTile>
+                <CounterTile titleOnClick={titleOnClick} background='#87929d' count={solved} title='Solved*' fullTitle={'Solved (30 days)'}></CounterTile>
             </Row>
         </Grid>
     )
 }
 
-const CounterTile = ({ count, background, title }: { count: number, background: string, color?: string, title: string }) => {
+const CounterTile = ({ count, background, title, fullTitle, titleOnClick }: { count: number, background: string, color?: string, title: string, fullTitle?: string, titleOnClick?: () => void }) => {
 
     return (<>
         {(count > 0) &&
             <Col size={2} style={{ width: 'initial', textAlign: 'center', margin: '0 6px' }}>
                 <LG isBold style={{ color: background }}>{count > 99 ? '99+' : count}</LG>
-                <SM>{title}</SM>
+                    <Tooltip content={(fullTitle ?? title) + ' = ' + count} placement='bottom-end'>
+                        <Anchor onClick={titleOnClick}><SM>{title}</SM></Anchor>
+                    </Tooltip>
             </Col>
         }
     </>)
--- packages/zendesk/components/TicketPanel.tsx
+++ packages/zendesk/components/TicketPanel.tsx
@@ -8,6 +8,7 @@ import { OrganizationServices } from "./OrganizationServices"
 import { RequesterTitle } from "./RequesterTitle"
 import { SpecialRequirements } from "./SpecialRequirements"
 import { RequesterTicketCounter } from "./RequesterTicketsCounter"
+import zafClient from "../sdk"
 
 
 type TicketPanelProps = {
@@ -19,11 +20,16 @@ export const TicketPanel = ({
     user,
     organization,
 }: TicketPanelProps) => {
+
+    const titleOnClick = () => {
+        zafClient.invoke('routeTo', 'user', user.id)
+    }
+
     return (
         <ImportantContactAlert user={user}>
             <RequesterTitle requester={user.name}></RequesterTitle>
             <hr />
-            <RequesterTicketCounter tickets={user.requestedTickets}></RequesterTicketCounter>
+            <RequesterTicketCounter tickets={user.requestedTickets} titleOnClick={titleOnClick}></RequesterTicketCounter>
             <NoSupportServices types={organization.services}></NoSupportServices>
             <ImportantContactTags isVip={user.isVip} userFlags={user.userFlags.filter((flag) => !(flag.type instanceof UserFlagTypeVip))}></ImportantContactTags>
             <OrganizationServices organizationServices={organization.services}></OrganizationServices>
