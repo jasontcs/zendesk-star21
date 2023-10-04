5e17a71479bbc05c79e087128ef37c4918306c58
Jason Tse <jasont@star21.com.au>
2023-10-04 11:25:13 +1100

    add refresh msg

--- packages/zendesk/components/AppLoader.tsx
+++ packages/zendesk/components/AppLoader.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import { Row, Col, Grid } from '@zendeskgarden/react-grid';
 import { Spinner } from '@zendeskgarden/react-loaders';
+import { SM } from '@zendeskgarden/react-typography';
+import { zafUtil } from '../common';
+import styled from 'styled-components';
+import { IGardenTheme } from '@zendeskgarden/react-theming';
 
-export const AppLoader = () => (
-  <Grid gutters={false}>
+export const AppLoader = () => {
+  const [showMessage, setShowMessage] = React.useState(false)
+
+  React.useEffect(() => {
+    (async function () {
+      await new Promise(f => setTimeout(f, 5000));
+      setShowMessage(true)
+    })()
+  }, [])
+
+  React.useEffect(() => { zafUtil.resizeWindow() })
+
+  const StyledSpacer = styled.div`
+  height: ${p => (p.theme as IGardenTheme).space.xxs};
+`;
+
+  return <Grid gutters={false}>
     <Row alignItems="center">
       <Col textAlign="center">
         <Spinner />
       </Col>
     </Row>
+    {
+      showMessage &&
+      <Row alignItems="center">
+        <Col textAlign="center">
+          <StyledSpacer></StyledSpacer>
+          <SM>Please refresh the whole page if needed</SM>
+        </Col>
+      </Row>
+    }
   </Grid>
-);
-
+}
