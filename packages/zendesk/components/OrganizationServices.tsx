a60fc2645d1704ac8aacafa7a37378a10691368e
Jason Tse <jasont@star21.com.au>
2023-10-04 11:25:42 +1100

    use accordion

--- packages/zendesk/components/OrganizationServices.tsx
+++ packages/zendesk/components/OrganizationServices.tsx
@@ -1,8 +1,9 @@
 import { Tag } from '@zendeskgarden/react-tags';
 import { Body, Cell, Row as TableRow, Table } from '@zendeskgarden/react-tables';
-import { MD, Span } from '@zendeskgarden/react-typography';
-import { ServiceEntity, ServiceType } from "../common/entity";
+import { LG, MD, Span } from '@zendeskgarden/react-typography';
+import { ServiceType } from "../common/entity";
 import React from 'react';
+import { Accordion } from '@zendeskgarden/react-accordions';
 
 
 
@@ -11,29 +12,38 @@ type OrganizationServicesProps = {
 }
 
 export const OrganizationServices = ({ organizationServices }: OrganizationServicesProps) => {
+    const validServices = organizationServices
+        .filter((type) => !type.noService)
     return (
-        <Table size="small">
-            <Body>
+        <Body>
+            <Accordion level={validServices.length} isCompact>
                 {
-                    organizationServices
-                        .filter((type) => !type.noService)
-                        .flatMap((type) => type.items.map((service) => [type, service] as [ServiceType, ServiceEntity])
-                        ).map((service, index) =>
-                            <TableRow isFocused={false} key={service[1].id} isStriped={index % 2 == 0} style={{ height: 'initial' }}>
-                                <Cell width="100px" style={{ textAlign: "center", paddingBlock: '2px' }}>
-                                    <Tag hue={service[0].color} style={{ width: "100%" }}>
-                                        <span>{service[0].title}</span>
-                                    </Tag>
-                                </Cell>
-                                <Cell style={{ textAlign: "right", paddingBlock: '2px' }}>
-                                    <MD>
-                                        <Span isBold>{service[1].name}</Span>
-                                    </MD>
-                                </Cell>
-                            </TableRow>
+                    validServices
+                        .map((type) =>
+                            <Accordion.Section>
+                                <Accordion.Header>
+                                    <Accordion.Label style={{ padding: '6px' }}>
+                                        <Tag hue={type.color} size='large' style={{ width: '100%' }}>
+                                            <Span>{type.title}</Span>
+                                        </Tag>
+                                    </Accordion.Label>
+                                </Accordion.Header>
+                                <Accordion.Panel style={{ padding: '0' }}>
+                                    <Table size="small">
+                                        {type.items.map((item, index) =>
+                                            <TableRow isFocused={false} key={item.id} isStriped={index % 2 == 0}>
+                                                <Cell style={{ textAlign: "left" }}>
+                                                    <MD>
+                                                        <Span isBold>{item.name}</Span>
+                                                    </MD>
+                                                </Cell>
+                                            </TableRow>)}
+                                    </Table>
+                                </Accordion.Panel>
+                            </Accordion.Section>
                         )
                 }
-            </Body>
-        </Table>
+            </Accordion>
+        </Body>
     )
 }
