055e989b96beda94df7e10b80117b86ec2fac188
Jason Tse <jasont@star21.com.au>
2023-10-05 13:20:47 +1100

    addd padding

--- packages/zendesk/components/OrganizationServices.tsx
+++ packages/zendesk/components/OrganizationServices.tsx
@@ -3,6 +3,8 @@ import { Body, Cell, Row as TableRow, Table } from '@zendeskgarden/react-tables'
 import { MD, Span } from '@zendeskgarden/react-typography';
 import { ServiceEntity, ServiceType } from "../common/entity";
 import React from 'react';
+import styled from 'styled-components';
+import { IGardenTheme } from '@zendeskgarden/react-theming';
 
 
 
@@ -10,30 +12,33 @@ type OrganizationServicesProps = {
     organizationServices: ServiceType[],
 }
 
+const StyledSpacer = styled.div`
+  height: ${p => (p.theme as IGardenTheme).space.xxs};
+`;
+
 export const OrganizationServices = ({ organizationServices }: OrganizationServicesProps) => {
     return (
-        <Table size="small">
-            <Body>
-                {
-                    organizationServices
+        <>
+            <StyledSpacer /><Table size="small">
+                <Body>
+                    {organizationServices
                         .filter((type) => !type.noService)
                         .flatMap((type) => type.items.map((service) => [type, service] as [ServiceType, ServiceEntity])
-                        ).map((service, index) =>
-                            <TableRow isFocused={false} key={service[1].id} isStriped={index % 2 == 0} style={{ height: 'initial' }}>
-                                <Cell width="100px" style={{ textAlign: "center", paddingBlock: '2px' }}>
-                                    <Tag hue={service[0].color} style={{ width: "100%" }}>
-                                        <span>{service[0].title}</span>
-                                    </Tag>
-                                </Cell>
-                                <Cell style={{ textAlign: "right", paddingBlock: '2px' }}>
-                                    <MD>
-                                        <Span isBold>{service[1].name}</Span>
-                                    </MD>
-                                </Cell>
-                            </TableRow>
-                        )
-                }
-            </Body>
-        </Table>
+                        ).map((service, index) => <TableRow isFocused={false} key={service[1].id} isStriped={index % 2 == 0} style={{ height: 'initial' }}>
+                            <Cell width="100px" style={{ textAlign: "center", paddingBlock: '2px' }}>
+                                <Tag hue={service[0].color} style={{ width: "100%" }}>
+                                    <span>{service[0].title}</span>
+                                </Tag>
+                            </Cell>
+                            <Cell style={{ textAlign: "right", paddingBlock: '2px' }}>
+                                <MD>
+                                    <Span isBold>{service[1].name}</Span>
+                                </MD>
+                            </Cell>
+                        </TableRow>
+                        )}
+                </Body>
+            </Table>
+        </>
     )
 }
